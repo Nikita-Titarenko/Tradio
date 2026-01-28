f4be0fb93c5f312a126113c917928fc6bdc6866e
Nikita Titarenko <nikitatitarenko81@gmail.com>
2026-01-28 16:17:15 +0200

    Add imports for home component

--- Tradio.Client/src/app/home.component.ts
+++ Tradio.Client/src/app/home.component.ts
@@ -1,8 +1,15 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'home',
-  templateUrl: 'home.component.html'
+  templateUrl: 'home.component.html',
+  imports: [
+    CommonModule,
+    RouterLink,
+    RouterOutlet
+  ]
 })
 
 export class HomeComponent { }
