da757d60ce262379e1551f9e36a89d433cc18ef8
Nikita Titarenko <nikitatitarenko81@gmail.com>
2026-01-28 14:35:58 +0200

    Add hero page

--- Tradio.Client/src/app/app.routes.ts
+++ Tradio.Client/src/app/app.routes.ts
@@ -2,6 +2,11 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
+    path: '',
+    loadComponent: () => import('./home.component').then(m => m.HomeComponent),
+    title: "Tradio"
+  },
+  {
     path: 'register',
     loadComponent: () => import('./auth/register/register.component').then(m => m.RegisterComponent),
     title: "Register"
