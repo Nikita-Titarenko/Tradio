b194f678fc1c3ecd4d80a902e5b409040e23bc13
Nikita Titarenko <nikitatitarenko81@gmail.com>
2026-01-28 16:17:40 +0200

    Add style and change markup for login and register page

--- Tradio.Client/src/app/auth/login/login.component.ts
+++ Tradio.Client/src/app/auth/login/login.component.ts
@@ -7,6 +7,7 @@ import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
+  styleUrl: './login.component.css',
   imports: [
     CommonModule,
     ReactiveFormsModule,
--- Tradio.Client/src/app/auth/register/register.component.ts
+++ Tradio.Client/src/app/auth/register/register.component.ts
@@ -7,6 +7,7 @@ import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
+  styleUrl: './register.component.css',
   imports: [
     CommonModule,
     ReactiveFormsModule,
