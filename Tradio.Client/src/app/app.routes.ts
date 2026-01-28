1a0119d5311f23bbde675a944cea7c3b7cb738cd
Nikita Titarenko <nikitatitarenko81@gmail.com>
2026-01-28 13:00:58 +0200

    Use single quotes instead of double.

--- Tradio.Client/src/app/app.routes.ts
+++ Tradio.Client/src/app/app.routes.ts
@@ -1,7 +1,14 @@
 import { Routes } from '@angular/router';
-import { LoginComponent } from './auth/login/login.component';
 
 export const routes: Routes = [
-  { path: 'register', loadComponent: () => import('./auth/register/register.component').then(m => m.RegisterComponent) },
-  {path: 'login', component: LoginComponent}
+  {
+    path: 'register',
+    loadComponent: () => import('./auth/register/register.component').then(m => m.RegisterComponent),
+    title: "Register"
+  },
+  {
+    path: 'login',
+    loadComponent: () => import('./auth/login/login.component').then(m => m.LoginComponent),
+    title: "Login"
+  }
 ];
--- Tradio.Client/src/app/auth/login/login.component.ts
+++ Tradio.Client/src/app/auth/login/login.component.ts
@@ -1,8 +1,8 @@
-import { Component } from "@angular/core";
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from "@angular/forms";
-import { Router, RouterModule } from "@angular/router"
-import { AuthService } from "../../core/services/auth.service";
-import { CommonModule } from "@angular/common";
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router, RouterModule } from '@angular/router'
+import { AuthService } from '../../core/services/auth.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
