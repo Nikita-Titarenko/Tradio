ff5fdb5d9f176638bad8d44ee30f8ab703ce8bdd
Nikita Titarenko <nikitatitarenko81@gmail.com>
2026-01-31 15:07:51 +0200

    Add services page

--- Tradio.Client/src/app/services/services/services.component.ts
+++ Tradio.Client/src/app/services/services/services.component.ts
@@ -1,17 +1,48 @@
 import { CommonModule } from "@angular/common";
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from "@angular/forms";
+import { CategoryModel } from "../../core/responses/category.model";
+import { CategoryService } from "../../core/services/category.service";
+import { ServiceService } from "../../core/services/service.service";
+import { ServiceListItemModule } from "../../core/responses/service-list-item.model";
 
 @Component({
     selector: 'services',
     templateUrl: './services.component.html',
     host: {
-      class: 'flex'
+      class: 'flex-column'
     },
     imports: [
-        CommonModule
+        CommonModule,
+        ReactiveFormsModule
     ]
 })
 
-export class ServicesComponent{
+export class ServicesComponent implements OnInit{
+    categories: CategoryModel[] = [];
+    services: ServiceListItemModule[] = [];
+    formGroup: FormGroup;
+    pageNumber = 1;
+    pageSize = 10;
+
+    constructor(private categoryService: CategoryService, private serviceService: ServiceService, formBuilder: FormBuilder){
+        this.formGroup = formBuilder.group({
+            selectedCategory: ['']
+        });
+        this.formGroup.get('selectedCategory')!.valueChanges.subscribe(value => {
+            this.serviceService.getServices(this.pageNumber, this.pageSize, {categoryId: value}).subscribe(data => {
+                this.categories = data;
+            })
+        });
+    }
+
+    ngOnInit(): void {
+        this.categoryService.getCategories().subscribe(data => {
+            this.categories = data;
+        });
+        this.serviceService.getServices(this.pageNumber, this.pageSize).subscribe(data => {
+            this.services = data;
+        })
+    }
 
 }
