a5167433b3acfc930325e846d6aeb4ae042c7ede
Nikita Titarenko <nikitatitarenko81@gmail.com>
2026-02-03 10:51:08 +0200

    fix: change filter logic for GetServicesAsync method

--- Tradio.Infrastructure/Repositories/ServiceRepository.cs
+++ Tradio.Infrastructure/Repositories/ServiceRepository.cs
@@ -13,7 +13,7 @@ namespace Tradio.Infrastructure.Repositories
         public async Task<IEnumerable<Service>> GetServicesAsync(int pageNumber, int pageSize, int? categoryId, int? countryId, int? cityId, string? subName)
         {
             return await _dbSet
-                .Where(s => categoryId == null || s.CategoryId == categoryId &&
+                .Where(s => (categoryId == null || s.CategoryId == categoryId) &&
                     (cityId == null || _dbContext.Users.Any(u => u.CityId == cityId && u.Services.Any(us => us.Id == s.Id))) &&
                     (countryId == null || _dbContext.Users.Any(u => u.City.CountryId == countryId && u.Services.Any(us => us.Id == s.Id))) &&
                     (subName == null || s.Name.Contains(subName)) &&
