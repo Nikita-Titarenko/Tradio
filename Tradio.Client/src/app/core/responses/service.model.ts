ee450b26aad24be995df3e067c12713d64df88cb
Nikita Titarenko <nikitatitarenko81@gmail.com>
2026-02-04 13:07:30 +0200

    feat: add service model

--- Tradio.Client/src/app/core/responses/service.model.ts
+++ Tradio.Client/src/app/core/responses/service.model.ts
@@ -1,1 +1,10 @@
-export interface ServiceModel {}
+export interface ServiceModel {
+  id: number;
+  name: string;
+  price: number;
+  description: string;
+  creationDateTime: Date;
+  categoryId: number;
+  applicationUserName: string;
+  categoryName: string;
+}
