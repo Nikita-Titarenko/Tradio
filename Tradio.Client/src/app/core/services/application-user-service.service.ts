7fcd541b9c3eafe7db2b47c5a5140c105d48de82
Nikita Titarenko <nikitatitarenko81@gmail.com>
2026-02-05 14:43:50 +0200

    feat: add ApplicationUserServiceService

---
+++ Tradio.Client/src/app/core/services/application-user-service.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ChatListItemModel } from '../responses/chat-list-item.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ApplicationUserServiceService {
+  private apiUrl = 'http://localhost:5188/api/ApplicationUserServices';
+
+  constructor(private http: HttpClient) {}
+
+  getReceivedServiceChats(): Observable<ChatListItemModel[]> {
+    return this.http.get<ChatListItemModel[]>(
+      `${this.apiUrl}/received-service`,
+    );
+  }
+}
