a63bc568ba68f83c7d99503f7425c2be688dc8d1
Nikita Titarenko <nikitatitarenko81@gmail.com>
2026-02-08 14:24:33 +0200

    feat: add BaseValidationError

---
+++ Tradio.Server/ValidationErrors/BaseValidationError.cs
@@ -0,0 +1,12 @@
+﻿namespace Tradio.Server.ValidationErrors;
+
+public class BaseValidationError
+{
+    public string Code { get; set; } = string.Empty;
+
+    public string Field { get; set; } = string.Empty;
+    
+    public string? Message { get; set; }
+
+    public Dictionary<string, string>? AdditionalData { get; set; }
+}
